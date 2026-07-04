0af33397aea4cafa9b574b4a6ca16bef832c74e3
Maria Alexis Sales <mariaalexissales@gmail.com>
2026-07-04 13:33:44 +0100

    added soda pack items

--- Contents/mods/Vanilla/42/media/lua/shared/packing.lua
+++ Contents/mods/Vanilla/42/media/lua/shared/packing.lua
@@ -1,13 +1,27 @@
 BUInv = BUInv or {}
 
-local function isFood(item)
-    return item and instanceof(item, "Food") and item:getDisplayCategory() == "Food" and item:getCurrentUses() > 0 and not item:isPackaged()
-end
+PACK_FLAVORS = {
+    ["BundleUp.BlueberryPack"]    = "SodaBlueberry",
+    ["BundleUp.BubblegumPack"]    = "SodaBubblegum",
+    ["BundleUp.SodaCanPack_Pop"]          = "SodaPop",
+    ["BundleUp.SodaCanPack_Lime"]         = "SodaLime",
+    ["BundleUp.SodaCanPack_Grape"]        = "SodaGrape",
+    ["BundleUp.SodaCanPack_Pineapple"]    = "SodaPineapple",
+    ["BundleUp.SodaCanPack_Strawberry"]   = "SodaStrawberry",
+}
 
-function BUInv.IsFavorite(items, result)
-    return not items:isFavorite()
-end
+---@param craftRecipeData CraftRecipeData
+---@param character IsoGameCharacter
+function BUInv.unpackSodaPack(craftRecipeData, character)
+    local sodaPack = craftRecipeData:getAllConsumedItems():get(0)
+    local sodaFluid = PACK_FLAVORS[sodaPack:getFullType()]
 
-function BUInv.IsFood(items, result)
-    return isFood(items)
+    local sodaType = Fluid.Get(sodaFluid)
+    local inventory = character:getInventory()
+    -- put the soda in the cans
+    for i = 1, 6 do
+        local can = inventory:AddItem("Base.SodaCan")
+        can:Empty()
+        can:AddFluid(sodaType, 1.0)
+    end
 end
