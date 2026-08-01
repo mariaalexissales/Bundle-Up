005e8291d4c18afc3817592ff62dca1f6dc58726
Maria Alexis Sales <mariaalexissales@gmail.com>
2026-08-01 06:57:30 -0500

    Add items

--- Contents/mods/Vanilla/42/media/lua/shared/BUpacking.lua
+++ Contents/mods/Vanilla/42/media/lua/shared/BUpacking.lua
@@ -1,13 +1,17 @@
+----------
+--ESTRAL--
+----------
+
 BUInv = BUInv or {}
 
 PACK_FLAVORS = {
-    ["BundleUp.BlueberrySP"]    = "SodaBlueberry",
-    ["BundleUp.BubblegumSP"]    = "SodaBubblegum",
-    ["BundleUp.LimeSP"]         = "SodaLime",
-    ["BundleUp.OrangeSP"]       = "SodaPop",
-    ["BundleUp.GrapeSP"]        = "SodaGrape",
-    ["BundleUp.PineappleSP"]    = "SodaPineapple",
-    ["BundleUp.StrawberrySP"]   = "SodaStrewberry",
+    ["BundleUp.BlueberrySP"]  = "SodaBlueberry",
+    ["BundleUp.BubblegumSP"]  = "SodaBubblegum",
+    ["BundleUp.LimeSP"]       = "SodaLime",
+    ["BundleUp.OrangeSP"]     = "SodaPop",
+    ["BundleUp.GrapeSP"]      = "SodaGrape",
+    ["BundleUp.PineappleSP"]  = "SodaPineapple",
+    ["BundleUp.StrawberrySP"] = "SodaStrewberry",
 }
 
 function BUInv.unpackSodaPack(craftRecipeData, character)
@@ -68,4 +72,54 @@ end
 
 function BUInv.testPackStrawberrySodaCan(item, character)
     return BU_isCanOfFlavor(item, "SodaStrewberry")
+end
+
+local function BU_worstFoodAge(items)
+    local worst = nil
+    for i = 0, items:size() - 1 do
+        local it = items:get(i)
+        if it and it:IsFood() then
+            local age = it:getAge()
+            if worst == nil or age > worst then
+                worst = age
+            end
+        end
+    end
+    return worst
+end
+
+function BUInv.packPerishable(craftRecipeData, character)
+    local worst = BU_worstFoodAge(craftRecipeData:getAllConsumedItems())
+    if worst == nil then
+        return
+    end
+    local created = craftRecipeData:getAllCreatedItems()
+    for i = 0, created:size() - 1 do
+        local pack = created:get(i)
+        if pack then
+            pack:getModData().buFoodAge = worst
+        end
+    end
+end
+
+function BUInv.unpackPerishable(craftRecipeData, character)
+    local consumed = craftRecipeData:getAllConsumedItems()
+    local age = nil
+    for i = 0, consumed:size() - 1 do
+        local pack = consumed:get(i)
+        if pack and pack:getModData().buFoodAge ~= nil then
+            age = pack:getModData().buFoodAge
+            break
+        end
+    end
+    if age == nil then
+        return
+    end
+    local created = craftRecipeData:getAllCreatedItems()
+    for i = 0, created:size() - 1 do
+        local food = created:get(i)
+        if food and food:IsFood() then
+            food:setAge(age)
+        end
+    end
 end
