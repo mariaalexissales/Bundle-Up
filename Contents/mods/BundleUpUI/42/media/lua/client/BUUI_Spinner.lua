36c549baab9a73de3d84b3e01084d99e784ec71b
Maria Alexis Sales <mariaalexissales@gmail.com>
2026-09-26 00:26:06 -0500

    refactor: spinner minus and plus share one builder

--- Contents/mods/BundleUpUI/42/media/lua/client/BUUI_Spinner.lua
+++ Contents/mods/BundleUpUI/42/media/lua/client/BUUI_Spinner.lua
@@ -9,6 +9,8 @@ BUUI_Spinner = ISPanel:derive("BUUI_Spinner")
 local BUTTON = 20
 local ENTRY = 34
 local GAP = 3
+local ICON = 10
+local MAX_TITLE_PAD = 12
 
 function BUUI_Spinner:new(x, y, width, height, target, onChange)
     local o = ISPanel:new(x, y, width, height)
@@ -24,16 +26,21 @@ function BUUI_Spinner:new(x, y, width, height, target, onChange)
     return o
 end
 
+local function BUUI_iconButton(spinner, x, texture, onClick)
+    local button = BUUI_Button:new(x, 0, BUTTON, BUTTON, "", spinner, onClick)
+    button.image = getTexture(texture)
+    button.forcedWidthImage = ICON
+    button.forcedHeightImage = ICON
+    button:initialise()
+    button:instantiate()
+    spinner:addChild(button)
+    return button
+end
+
 function BUUI_Spinner:createChildren()
     ISPanel.createChildren(self)
 
-    self.less = BUUI_Button:new(0, 0, BUTTON, BUTTON, "", self, BUUI_Spinner.onLess)
-    self.less.image = getTexture("media/ui/Entity/BTN_Minus_Icon_48x48.png")
-    self.less.forcedWidthImage = 10
-    self.less.forcedHeightImage = 10
-    self.less:initialise()
-    self.less:instantiate()
-    self:addChild(self.less)
+    self.less = BUUI_iconButton(self, 0, "media/ui/Entity/BTN_Minus_Icon_48x48.png", BUUI_Spinner.onLess)
 
     self.entry = ISTextEntryBox:new("1", BUTTON + GAP, 0, ENTRY, BUTTON)
     self.entry.font = UIFont.Small
@@ -44,16 +51,12 @@ function BUUI_Spinner:createChildren()
     self.entry.target = self
     self:addChild(self.entry)
 
-    self.more = BUUI_Button:new(BUTTON + ENTRY + GAP * 2, 0, BUTTON, BUTTON, "", self, BUUI_Spinner.onMore)
-    self.more.image = getTexture("media/ui/Entity/BTN_Plus_Icon_48x48.png")
-    self.more.forcedWidthImage = 10
-    self.more.forcedHeightImage = 10
-    self.more:initialise()
-    self.more:instantiate()
-    self:addChild(self.more)
+    self.more = BUUI_iconButton(self, BUTTON + ENTRY + GAP * 2,
+        "media/ui/Entity/BTN_Plus_Icon_48x48.png", BUUI_Spinner.onMore)
 
-    self.maxButton = BUUI_Button:new(BUTTON * 2 + ENTRY + GAP * 3, 0, BUTTON, BUTTON, getText("IGUI_BUUI_Max"), self, BUUI_Spinner.onMax)
-    self.maxButton:sizeToTitle(12)
+    self.maxButton = BUUI_Button:new(BUTTON * 2 + ENTRY + GAP * 3, 0, BUTTON, BUTTON,
+        getText("IGUI_BUUI_Max"), self, BUUI_Spinner.onMax)
+    self.maxButton:sizeToTitle(MAX_TITLE_PAD)
     self.maxButton:initialise()
     self.maxButton:instantiate()
     self:addChild(self.maxButton)
